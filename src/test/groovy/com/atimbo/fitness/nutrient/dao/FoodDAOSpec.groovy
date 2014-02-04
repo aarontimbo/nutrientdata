9098bdf336315b394a5d78e4f92c8fb402932261
Aaron Timbo <aaron.timbo@gmail.com>
2014-02-03 20:41:18 -0600

    Update startup banner and add failing test to check that cloudbees jenkins is working.

--- src/test/groovy/com/atimbo/fitness/nutrient/dao/FoodDAOSpec.groovy
+++ src/test/groovy/com/atimbo/fitness/nutrient/dao/FoodDAOSpec.groovy
@@ -3,6 +3,8 @@ package com.atimbo.fitness.nutrient.dao
 import com.atimbo.fitness.nutrient.domain.Food
 import com.atimbo.fitness.nutrient.domain.FoodGroup
 
+import javax.persistence.EntityNotFoundException
+
 class FoodDAOSpec extends DatabaseSpecification {
 
     FoodDAO foodDAO
@@ -10,7 +12,6 @@ class FoodDAOSpec extends DatabaseSpecification {
 
     def setup() {
         foodGroupDAO= new FoodGroupDAO(sessionFactory)
-
         foodDAO = new FoodDAO(sessionFactory)
     }
 
@@ -40,4 +41,25 @@ class FoodDAOSpec extends DatabaseSpecification {
         food == expectedFood
 
     }
+
+    void 'retrieving a food that does not exist throws an error'() {
+        given:
+        FoodGroup foodGroup = new FoodGroup(description: 'meat')
+        foodGroupDAO.saveOrUpdate(foodGroup)
+        Food expectedFood = new Food(
+                longDescription: 'meat',
+                shortDescription: 'meat',
+                foodGroup: foodGroup
+        )
+        foodDAO.saveOrUpdate(expectedFood)
+        assert expectedFood.id
+
+        when:
+        foodDAO.findById(expectedFood.id)
+
+        then:
+        thrown(EntityNotFoundException)
+
+    }
+
 }
